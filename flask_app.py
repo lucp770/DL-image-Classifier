9bdf851acc8e260cdd83cc0eaecac534b3fcae86
Lucas <lucp770@gmail.com>
2023-01-21 19:53:18 -0300

    implementing the POST method to the server

--- flask_app.py
+++ flask_app.py
@@ -15,9 +15,12 @@ import imageio as iio
 
 app = Flask(__name__)
 
+
+models = ['AlexNet','VGG','ResNet','SqueezeNet','DenseNet','Inception v3']
+
 @app.route("/")
 def homepage():
-	return render_template("main.html")
+	return render_template("main.html", models = models)
 
 @app.route("/", methods = ['POST'])
 def receive_image():
@@ -25,9 +28,19 @@ def receive_image():
 
 	# get the data stream from the post request
 	image_data = request.get_data()
+	# print(image_data);
+	for i in request.files:
+		print(i)
+	
+	# for i in dir(request):
+	# 	print(i)
+
+	# print('\n \n propriedades do request getdata')
+	# for j in dir(image_data):
+	# 	print(j)
 
 	# convert the data stream to an array
-	image = iio.imread(data1)
+	# image = iio.imread(data1)
 
 	# now insert here the code for image classification
 	
@@ -40,7 +53,8 @@ if __name__ =="__main__":
 # 		-put the arrow dow in the menu v
 # 		-put the menu going down
 
-# -manipulate the image in the background so it transform in an array.
+# -manipulate the image in the background so it transform in an array. V
+
 # -no botao submit, incluir uma janela de carregamento no estilo futurista
 # -elaborar modelo de ML baseado em CNN para classificação da imagem colocada pelo usuario
 # -implementar a classificação e um retorno para o usuario no método 
--- static/main.js
+++ static/main.js
@@ -1,9 +1,18 @@
 const imgContainer = document.querySelector('.image-container');
 const contButton = document.querySelector('.continue-button');
 
+const models = document.querySelectorAll('.model');
+const selectModelBtn = document.querySelector('.select-model');
+
+const slideMenu = document.querySelector('.model-selection');
+const arrowLogo = document.querySelector('.fa-solid');
+
+const modelInfo = document.querySelector('.model-information');
+
+
 let file = undefined;
 let errorMessage;
-let Model = 'VGG';
+let Model = '';
 
 
 function showFile(input){
@@ -35,9 +44,11 @@ function postData(){
 
         // execute fetch to the server, passing header and body.
         let options = {method: "POST",
-        body: file,
-        headers: {'content-type':'image/png'}
+        body: {file:file, model: Model},
+        headers: {'content-type':'multipart/form-data'}
         }
+
+        delete options.headers['Content-Type'];
 
         let r = fetch('/',options);
         console.log(r);
@@ -52,7 +63,75 @@ function postData(){
     }
 }
 
+
+function sendData(data){
+    const formData = new FormData();
+
+    for(const name in data){
+        formData.append(name, data[name]);
+    }
+
+    let options = {
+    method: "POST",
+    body: formData
+    }
+
+    let r = fetch('/',options);
+    r.then(res=>console.log(res));
+
+}
+
+// event listeners
+
 contButton.addEventListener('click',(e)=>{
     e.preventDefault();
     postData();
+
+})
+
+models.forEach(model=>{
+    model.addEventListener('click', ()=>{
+        console.log(model.innerText);
+        // define the model to be selected
+        Model = model.innerText;
+        console.log('variable Model value: ', Model);
+
+        modelInfo.innerText = 'The model selected is ' + Model;
+
+       
+        // remove the selected class from others and add only on the clicked button.
+        models.forEach(modelo=>{
+            if(modelo.classList.contains('selected')){
+                modelo.classList.remove('selected');
+            } 
+        })
+        
+         // change the syle, to indicate the selection.
+        model.classList.add('selected');
+
+        })
+})
+
+selectModelBtn.addEventListener('click', ()=>{
+
+    // check if is visible
+    let classes = slideMenu.classList;
+    // console.log(classes.contains('visible'));
+    if(classes.contains('hidden')){
+        console.log('showMenu');
+        classes.remove('hidden');
+
+        // change the logo to up arrow.
+        arrowLogo.classList.remove('fa-chevron-down');
+        arrowLogo.classList.add('fa-chevron-up')
+    }
+    else{
+        console.log('hide Menu')
+
+        classes.add('hidden');
+
+        // change the logo to up down.
+        arrowLogo.classList.remove('fa-chevron-up');
+        arrowLogo.classList.add('fa-chevron-down')
+    }
 })
